eae0a19cd69d033e85e828b8d75f60316bfdbb86
Leo Spratt <enchant97@users.noreply.github.com>
2026-06-19 12:23:05 +0100

    init

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter";
+import { Roboto } from 'next/font/google';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../theme';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+});
+
+export const metadata: Metadata = {
+  title: "Wiki",
+  description: "A wiki",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${roboto.variable}`}>
+      <body>
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline enableColorScheme={true} />
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
---
+++ theme.ts
@@ -0,0 +1,14 @@
+'use client';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  cssVariables: true,
+  typography: {
+    fontFamily: 'var(--font-roboto)',
+  },
+  colorSchemes: {
+    dark: true,
+  },
+});
+
+export default theme;
