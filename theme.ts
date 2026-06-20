a67a48e44b4ae6c0f505ec23441f40615189ae62
Leo Spratt <enchant97@users.noreply.github.com>
2026-06-20 23:16:21 +0100

    use native css color for mui

--- theme.ts
+++ theme.ts
@@ -2,7 +2,9 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
-  cssVariables: true,
+  cssVariables: {
+    nativeColor: true,
+  },
   typography: {
     fontFamily: 'var(--font-roboto)',
   },
