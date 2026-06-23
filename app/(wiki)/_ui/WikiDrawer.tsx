a5f09b8e990625d04eeb8aa50ce62dba16afba07
Leo Spratt <enchant97@users.noreply.github.com>
2026-06-23 19:30:32 +0100

    filter empty string from slug parts

--- app/(wiki)/_ui/WikiDrawer.tsx
+++ app/(wiki)/_ui/WikiDrawer.tsx
@@ -18,10 +18,18 @@ import useSWR from 'swr';
 
 const drawerWidth = 240;
 
+function getSlugPartsFromLayoutSegments(): string[] {
+  let parts = (useSelectedLayoutSegments()[1] ?? "").split("/")
+  if (parts[0] == "") {
+    parts.pop()
+  }
+  return parts
+}
+
 export default function WikiDrawer(props: PropsWithChildren) {
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const [isClosing, setIsClosing] = React.useState(false);
-  const slugParts = (useSelectedLayoutSegments()[1] ?? "").split("/")
+  const slugParts = getSlugPartsFromLayoutSegments()
   // TODO find a way of rendering tree on server-side
   const { data: relPageSlugs } = useSWR(
     ["relPageSlugs", slugParts],
