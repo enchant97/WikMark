19445948389f98d9cfaa41a40043e2183aca3555
Leo Spratt <enchant97@users.noreply.github.com>
2026-07-05 20:35:10 +0100

    add missing metadata to client rendered page

--- src/app/(wiki)/-/[[...slug]]/_ui/RenderedPageClient.tsx
+++ src/app/(wiki)/-/[[...slug]]/_ui/RenderedPageClient.tsx
@@ -2,8 +2,10 @@
 import { renderMarkdown } from "@/lib/helpers"
 import { useEffect, useState } from "react"
 import RenderedPageLoading from "./RenderedPageLoading"
+import { PageMetadata } from "@/lib/types";
+import PageFooter from "./PageFooter";
 
-export default function RenderedPageClient(props: { content: string, title: string }) {
+export default function RenderedPageClient(props: { content: string, title: string, metadata: PageMetadata }) {
   const [rendered, setRendered] = useState<string | null>(null)
   useEffect(() => {
     const startTime = performance.now()
@@ -20,6 +22,7 @@ export default function RenderedPageClient(props: { content: string, title: stri
         {/* hiding the header until content load is a stylistic choice */}
         <h1>{props.title}</h1>
         <div className="wikiProse" dangerouslySetInnerHTML={{ __html: rendered }}></div>
+        <PageFooter metadata={props.metadata} />
       </>
     }
     </>
