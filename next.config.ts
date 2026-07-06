86f460496c4675368f4b83ef785445de37e27725
Leo Spratt <enchant97@users.noreply.github.com>
2026-07-06 11:36:32 +0100

    add config to support production export
    configs prevent OOM errors, tsc should be run on CI before build to
    ensure there are no breaking errors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
+  output: "standalone",
+  typescript: {
+    ignoreBuildErrors: true, // prevents OOM errors on build
+  },
+  productionBrowserSourceMaps: false, // prevents OOM errors on build
+  experimental: {
+    serverSourceMaps: false, // prevents OOM errors on build
+  },
+  enablePrerenderSourceMaps: false, // prevents OOM errors on build
 };
 
 export default nextConfig;
