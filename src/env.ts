4c4b12d7c0d332d14209c41e66766bf349e0e98d
Leo Spratt <enchant97@users.noreply.github.com>
2026-07-07 11:17:18 +0100

    add dev/build validation skip

--- src/env.ts
+++ src/env.ts
@@ -1,19 +1,25 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import * as z from "zod";
 
+const SKIP_VALIDATION = process.env.ENABLE_SKIP_VALIDATION === "UNSAFE_ENABLE"
+
 const isAbsolute = (s: string) => s.startsWith("/")
 const isValidSQLitePath = (s: string) => {
   return s === ":memory:"
-    ? true
+    ? SKIP_VALIDATION
     : isAbsolute(s)
 }
+
+const authSecretSchema = SKIP_VALIDATION
+  ? z.base64().min(32).optional()
+  : z.base64().min(32)
 
 const env = createEnv({
   server: {
     WIKI_PATH: z.coerce.string().refine(isAbsolute, { error: "Expected an absolute path" }),
     DB_PATH: z.coerce.string().refine(isValidSQLitePath, { error: "Expected an absolute path" }),
     SEARCH_DB_PATH: z.coerce.string().refine(isValidSQLitePath, { error: "Expected an absolute path" }),
-    AUTH_SECRET: z.base64().min(32),
+    AUTH_SECRET: authSecretSchema,
     META_TITLE: z.string().default("WikMark"),
     META_DESCRIPTION: z.string().default("A WikMark Wiki"),
     ENABLE_SIGNUP: z.stringbool().default(true),
@@ -32,6 +38,7 @@ const env = createEnv({
   },
   experimental__runtimeEnv: {},
   emptyStringAsUndefined: true,
+  skipValidation: SKIP_VALIDATION,
 })
 
 export default env
