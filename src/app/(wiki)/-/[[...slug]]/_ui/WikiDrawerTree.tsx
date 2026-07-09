5afc7a4fd8845b5e246de104b7c36cb3f5a93b62
Leo Spratt <enchant97@users.noreply.github.com>
2026-07-09 11:38:21 +0100

    nicer tree navigation

--- src/app/(wiki)/-/[[...slug]]/_ui/WikiDrawerTree.tsx
+++ src/app/(wiki)/-/[[...slug]]/_ui/WikiDrawerTree.tsx
@@ -3,6 +3,9 @@ import { joinSlugParts } from "@/lib/helpers";
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from "@mui/material/ListItemIcon";
+import SubdirectoryArrowLeftIcon from '@mui/icons-material/SubdirectoryArrowLeft';
+import ListItemText from "@mui/material/ListItemText";
 
 interface Props {
   relSlugs: string[]
@@ -11,13 +14,27 @@ interface Props {
 
 export default function WikiDrawerTree({ relSlugs, slugParts }: Props) {
   return (
-    <List>
+    <List
+      component="nav"
+      dense={true}
+    >
+      {slugParts.length !== 0 && (
+        <ListItem key={""}>
+          <ListItemButton
+            LinkComponent={NextLink}
+            href={`/-/${joinSlugParts(slugParts.slice(0, -1))}`}
+          >
+            <ListItemIcon><SubdirectoryArrowLeftIcon /></ListItemIcon>
+            <ListItemText primary={slugParts.at(-2) ?? "Home"} />
+          </ListItemButton>
+        </ListItem>
+      )}
       {relSlugs.map(slug => (
         <ListItem key={slug}>
           <ListItemButton
             LinkComponent={NextLink}
             href={`/-/${joinSlugParts([...slugParts, slug])}`}
-          >{slug}</ListItemButton>
+          ><ListItemText primary={slug} /></ListItemButton>
         </ListItem>
       ))}
     </List>
